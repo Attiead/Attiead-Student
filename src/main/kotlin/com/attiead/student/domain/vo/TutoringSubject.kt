314dd8b9015843d05009bf72b7eea71f7e9feee4
sanghoon <nonsleep79@gmail.com>
2024-03-03 21:27:25 +0900

    remove ;

--- src/main/kotlin/com/attiead/student/domain/vo/TutoringSubject.kt
+++ src/main/kotlin/com/attiead/student/domain/vo/TutoringSubject.kt
@@ -12,5 +12,5 @@ enum class TutoringSubject(val description: String) {
     FINE_ARTS("실기 - 그림"),
     VOCAL_MUSIC("실기 - 성악"),
     INSTRUMENTAL_MUSIC("실기 - 악기"),
-    PHYSICAL_EDUCATION("실기 - 체육");
+    PHYSICAL_EDUCATION("실기 - 체육")
 }
