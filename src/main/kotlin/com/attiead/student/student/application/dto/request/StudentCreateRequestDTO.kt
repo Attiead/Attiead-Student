9fa31f935742b645eee73c6ea3c43fcda544cefd
sanghoon <nonsleep79@gmail.com>
2024-03-24 22:08:48 +0900

    Refactor: @NotNull 제거

--- src/main/kotlin/com/attiead/student/student/application/dto/request/StudentCreateRequestDTO.kt
+++ src/main/kotlin/com/attiead/student/student/application/dto/request/StudentCreateRequestDTO.kt
@@ -2,14 +2,12 @@ package com.attiead.student.student.application.dto.request
 
 import com.attiead.student.domain.table.Student
 import com.attiead.student.domain.vo.MentoringTopic
-import jakarta.validation.constraints.NotNull
 import java.time.LocalDateTime
 
 data class StudentCreateRequestDTO(
 
     val sid: String,
 
-    @field:NotNull
     val uid: String,
 
     val schoolEmail: String,
